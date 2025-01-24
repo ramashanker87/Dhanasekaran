8a3f582883703b4d272754dba0d036bfc4b402e4
Dhanasekaran <dhana880@yahoo.com>
2025-01-23 21:57:06 -0800

    Day 04 - project name and app name updated.

---
+++ Day04/dhana1-exception/src/main/java/com/dhana/app/custom/AgeValidationException.java
@@ -0,0 +1,7 @@
+package com.dhana.app.custom;
+
+public class AgeValidationException extends Exception {
+    public AgeValidationException(String message) {
+        super(message);
+    }
+}
---
+++ Day04/dhana1-exception/src/main/java/com/dhana/app/custom/TestNameValidation.java
@@ -0,0 +1,21 @@
+package com.dhana.app.custom;
+
+public class TestNameValidation {
+    public static void main(String[] args) {
+        TestNameValidation test=new TestNameValidation();
+        try{
+            test.validateName("Sekar");
+        }catch(NameValidationException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }finally{
+            System.out.println("Name Validation Exception Executed");
+        }
+    }
+
+    public void validateName(String name) throws NameValidationException {
+        if (name != "Dhana") {
+            throw new NameValidationException("Name must be Dhana.");
+        }
+    }
+}
