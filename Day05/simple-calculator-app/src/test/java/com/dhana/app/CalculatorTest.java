05089a955929f02dd175221d82487ad70215eda1
Dhanasekaran <dhana880@yahoo.com>
2025-01-26 21:17:16 -0800

    Day 05 CalcutorTest app

---
+++ Day05/simple-calculator-app/src/test/java/com/dhana/app/CalculatorTest.java
@@ -0,0 +1,28 @@
+package com.dhana.app;
+
+import.junit.framework.Testsuite;
+import.org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        double result = calculator.add(5,5);
+        assert result==10;
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        double result = calculator.subtract(4,2);
+        assert result==2;
+    }
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        double result = calculator.multiply(5,5);
+        assert result==25;
+    }
+
+}
