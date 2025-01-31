df5284d395114f8db893709e8b51ca06fa0cbc4e
Dhana889 <kvs.dhana@gmail.com>
2025-01-31 13:14:16 +0530

    Day 08 Assignment update, app created, yet to add auth tokens

--- Day08/patient-management-app/src/main/java/com/dhana/app/service/PatientService.java
+++ Day08/patient-management-app/src/main/java/com/dhana/app/service/PatientService.java
@@ -1,4 +1,36 @@
 package com.dhana.app.service;
 
+import com.dhana.app.model.Patient;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class PatientService {
+    Map<String, Patient> patients = new HashMap<String, Patient>();
+
+    public Patient createPatient(Patient patient) {
+        patients.put(patient.getId(), patient);
+        return patient;
+    }
+
+    public Patient updatePatient(String id, int age) {
+        Patient patient = patients.get(id);
+        patient.setAge(age);
+        return patient;
+    }
+
+    public Map<String,Patient> readAllPatient() {
+        return patients;
+    }
+
+    public void deletePatient(String id) {
+        patients.remove(id);
+    }
+
+    public Patient readPatientByName(String name) {
+        Patient result = patients.get(name);
+        return result;
+    }
 }
