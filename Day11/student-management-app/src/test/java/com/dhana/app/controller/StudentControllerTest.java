a8e7c6e760171b25efedc85127854ecb65069b1e
admin <rama@gail.com>
2025-02-06 00:35:08 -0800

    Day11- Springboot application student management app updated

--- Day11/student-management-app/src/test/java/com/dhana/app/controller/StudentControllerTest.java
+++ Day11/student-management-app/src/test/java/com/dhana/app/controller/StudentControllerTest.java
@@ -7,10 +7,13 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.client.ExpectedCount;
 
-import static org.mockito.ArgumentMatchers.anySet;
-import static org.mockito.ArgumentMatchers.anyString;
+import java.util.*;
+
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.ExpectedCount.times;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StudentControllerTest {
@@ -32,4 +35,19 @@ public class StudentControllerTest {
         assert resultStudent.getAge()==(15);
         assert resultStudent.getGender().equals("F");
     }
+
+    @Test
+    public void testGetStudents() {
+
+        Map<String, Student> studentMap = new HashMap<>();
+        Student student1 = new Student(1, "John", "ABCSchool", 15, "F");
+        Student student2 = new Student(2, "Bane", "ABDSchool", 18, "M");
+        Student student3 = new Student(3, "Jane", "ABESchool", 17, "F");
+        studentMap.put(student1.getName(), student1);
+        when(studentController.read()).thenReturn(studentMap.values());
+        Iterable<Student> resultStudent = studentController.read();
+        assert resultStudent!=null;
+    }
+
+
 }
