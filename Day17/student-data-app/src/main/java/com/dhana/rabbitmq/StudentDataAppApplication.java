6caee4834fdbbb60d2adf76b05dadbf219cb9148
Dhana889 <kvs.dhana@gmail.com>
2025-02-12 10:39:16 +0530

    Day 17 student app created

---
+++ Day17/student-data-app/src/main/java/com/dhana/rabbitmq/StudentDataAppApplication.java
@@ -0,0 +1,13 @@
+package com.dhana.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDataAppApplication.class, args);
+	}
+
+}
---
+++ Day17/student-data-app/src/test/java/com/dhana/rabbitmq/StudentDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhana.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
